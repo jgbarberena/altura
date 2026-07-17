a90a3a21269574bc2c0576a4f3d25c25ea15c162
jgbarberena <jgbarberena@gmail.com>
2026-07-17 22:56:40 +0200

    revert: dlgGasto bookedDate vuelve a issueDate (correcto fiscalmente)

--- admin/js/dlg-gasto.js
+++ admin/js/dlg-gasto.js
@@ -377,7 +377,7 @@ async function _abrirModal(doc, provider, onGuardado) {
             window._dlgRecalcSimp()
             total      = parseFloat(document.getElementById('dlg-total-simp')?.value)
             invNum     = get('dlg-invoice-number') || `T-${Date.now()}`
-            bookedDate = hoy
+            bookedDate = issueDate
             category   = 'otros'
             dedPct     = 100
             isCapital  = false
